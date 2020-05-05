1be4c74beb4639203118f8e31b5a13ba57b488bf
Diep Thuy <boychaos9x@gmail.com>
2020-05-05 11:15:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(arr1, arr2){
+  const resultArr= []
+  for (let i =0 ; i< arr1.length; i++){
+    resultArr.push(`${arr1[i]} plays ${arr2[i]}`)
+  }
+  return resultArr;
+}
+
+function johnLennonFacts (arr){
+  let i=0;
+  do {
+    arr[i]= arr[i]+'!!!';
+    i++;
+  }
+  while (i< arr.length)
+  return arr;
+}
+function iLoveTheBeatles(number){
+  const resultArr=[];
+  let i=0;
+  do {
+    resultArr[i]= 'I love the Beatles!'
+  }
+  while (number<=15)
+};
